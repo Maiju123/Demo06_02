cc95db344d98d2f6c47b02936c3cd7f0395dae7a
Maiju Schreck <K2322@student.jamk.fi>
2016-02-17 11:07:55 +0200

    Demo07 Tunti tehtäviä

---
+++ CarApp/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CarApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void trucksButton_Click(object sender, RoutedEventArgs e)
+        {   
+            //Haetaan käyttöliittymästä trucksTextBlock sisältämä teksti
+            string trucksString = trucksTextBlock.Text; //"0"
+            int trucks = int.Parse(trucksString); //0
+            //kuorma-autoja yksi enemmän
+            trucks++; // TAI trucks = trucks + 1;
+            //näytetään uusi lukumäärä näytöllä
+            trucksTextBlock.Text = trucks.ToString(); // 1 --> "1"
+        }
+
+        private void trucksTextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void carsTextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void carsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Haetaan käyttöliittymästä carTextBlock sisältämän tekstin
+            string carsString = carsTextBlock.Text;
+            int cars = int.Parse(carsString);
+
+            cars = cars + 1;
+
+           carsTextBlock.Text = cars.ToString(); 
+        }
+    }
+}
---
+++ CurrencyCalculator/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CurrencyCalculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void euroButton_Click(object sender, RoutedEventArgs e)
+        {
+            string eurosString = euroTextBlock.Text;
+            double euros = double.Parse(eurosString);
+
+            euros = euros * 5.94573;
+
+            euroTextBlock.Text = euros.ToString();
+        }
+
+        private void markkaButton_Click(object sender, RoutedEventArgs e)
+        {
+            string markkasString = markkaTextBlock.Text;
+            double markkas = double.Parse(markkasString);
+
+            markkas = markkas / 5.94573;
+
+            markkaTextBlock.Text = markkas.ToString();
+        }
+    }
+}
